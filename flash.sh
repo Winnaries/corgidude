2a5194e2c4de63a22cd4352be60d8beadb74ad2e
winnaries <w.winnaries@gmail.com>
2021-02-19 17:46:41 +0700

    wip: Get the OV2640 to stream data into a static variable

--- flash.sh
+++ flash.sh
@@ -4,6 +4,6 @@ if [[ $1 == "release" ]]; then
     cargo objcopy --release --bin $2 -- -O binary target/riscv64gc-unknown-none-elf/release/$2.bin
     kflash -S -t -p /dev/tty.usbserial-14410 -B dan -b 150000 target/riscv64gc-unknown-none-elf/release/$2.bin
 else 
-    cargo objcopy --bin $2 -- -O binary target/riscv64gc-unknown-none-elf/debug/$2.bin
-    kflash -S -t -p /dev/tty.usbserial-14410 -B dan -b 150000 target/riscv64gc-unknown-none-elf/debug/$2.bin
+    cargo objcopy --bin $1 -- -O binary target/riscv64gc-unknown-none-elf/debug/$1.bin
+    kflash -S -t -p /dev/tty.usbserial-14410 -B dan -b 150000 target/riscv64gc-unknown-none-elf/debug/$1.bin
 fi
